0a1c5e1aacdbeb41b8aa582d6c3c6fc40e441fa3
Frederic Perez <fredericpcx@gmail.com>
2015-03-02 15:37:46 +0100

    Petty extension (to play w/github Windows app)

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ main()
 	const std::string welcome = R"(Hello,
 world!)";
 	std::cout << welcome << std::endl;
+	std::clog << welcome << " (this time to std::clog)" << std::endl;
 	return EXIT_SUCCESS;
 }
 
