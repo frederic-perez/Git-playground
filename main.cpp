7d0e33d78b3c4b674850702951c317accdaf7f31
Frederic Perez <frederic-perez@users.noreply.github.com>
2018-05-13 19:46:38 +0200

    master :art: Petty changes (tab>spaces, etc.)

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
-// --
-
 #include <iostream>
 #include <string>
 
 int
 main()
 {
-	const std::string welcome = R"(Hello,
+  const std::string welcome = R"(Hello,
 world!)";
-	std::cout << welcome << std::endl;
-	std::clog << welcome << " (this time to std::clog)" << std::endl;
-	return EXIT_SUCCESS;
+  std::cout << welcome << std::endl;
+  std::clog << welcome << " (this time to std::clog)" << std::endl;
+  return EXIT_SUCCESS;
 }
-
-// -- eof
